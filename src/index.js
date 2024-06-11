71ef119d530c01470855c305bee92b21dc9c84a0
Guffy-san <vinaybasargekar13@gmail.com>
2024-06-11 11:36:24 +0530

    added webpack

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-import "./styles.css";
+import joke from './tp.js';
 
-import makeHomePage from "./home";
-import makeMenuPage from "./menu";
-import makeAboutPage from "./about";
-
-function component() {
-	
-	const container = document.querySelector("#content");
-	const homeBtn = document.querySelector("#homeBtn");
-	const menuBtn = document.querySelector("#menuBtn");
-	const aboutBtn = document.querySelector("#aboutBtn");
-
-	homeBtn.addEventListener("click", () => {
-		container.textContent = "";
-		container.appendChild(makeHomePage());
-	});
-	menuBtn.addEventListener("click", () => {
-		container.textContent = "";
-		container.appendChild(makeMenuPage());
-	});
-	aboutBtn.addEventListener("click", () => {
-		container.textContent = "";
-		container.appendChild(makeAboutPage());
-	});
-
-	const homePage = makeHomePage();
-	container.appendChild(homePage);
-	return container;
-}
-
-document.body.appendChild(component());
+console.log(joke());
---
+++ src/tp.js
@@ -0,0 +1,5 @@
+function joke() {
+	return "joke";
+}
+
+export default joke;
